af6cb2a462a22921b77d6a3d6fef76c0693df3bf
Tyler Hawkins <tylerh@adobe.com>
2021-12-07 15:28:48 -0700

    refactor: organizes order of Storybook files

---
+++ src/00Welcome.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default {
+  title: 'Welcome',
+};
+
+export const Welcome = () => (
+  <>
+    <h1>👋 Welcome to React Tables and Grids</h1>
+    <p>
+      This Storybook playground showcases various tables and grids built in
+      React. Best of all, the content is accessible. 🎉
+    </p>
+    <p>
+      All the content and design patterns in this project meet the WCAG 2.1 AA
+      success criteria and adhere to the WAI-ARIA Authoring Practices guidelines
+      for creating accessible widgets. 🎊
+    </p>
+  </>
+);
