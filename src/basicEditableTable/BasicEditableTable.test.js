c37d579b12c108dcce63fe3e09dd77e7018e1ee8
Tyler Hawkins <tylerh@adobe.com>
2021-12-09 12:01:01 -0700

    test: adds tests for Edit Mode in editable table

--- src/basicEditableTable/BasicEditableTable.test.js
+++ src/basicEditableTable/BasicEditableTable.test.js
@@ -55,7 +55,7 @@ describe('BasicEditableTable', () => {
     });
   });
 
-  describe('Read-Only Mode', () => {
+  describe('View Mode', () => {
     it('renders each table cell as a button', () => {
       render(<BasicEditableTable tableData={basicPokemonData} />);
 
@@ -157,6 +157,417 @@ describe('BasicEditableTable', () => {
       expect(screen.getAllByRole('button').length).toBe(
         numberOfTableBodyCells - numberOfColumns
       );
+    });
+  });
+
+  describe('Edit Mode', () => {
+    const enterEditMode = () => {
+      const firstCellInTable = screen.getByRole('button', {
+        name: '1 - Press Enter to edit',
+      });
+
+      userEvent.tab();
+
+      userEvent.type(firstCellInTable, '{enter}');
+    };
+
+    describe('table navigation', () => {
+      describe('Tab key', () => {
+        it('moves focus right by one cell when the user presses Tab', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          const secondTextInputInFirstRow =
+            screen.getByDisplayValue('Bulbasaur');
+          const thirdTextInputInFirstRow =
+            screen.getAllByDisplayValue('Grass')[0];
+          const fourthTextInputInFirstRow = screen.getAllByDisplayValue(
+            'One of the three starter Pokémon'
+          )[0];
+
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(secondTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(thirdTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(fourthTextInputInFirstRow).toHaveFocus();
+        });
+
+        it('moves focus to the first cell in the next row when the user presses Tab on the last cell in a row', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          const secondTextInputInFirstRow =
+            screen.getByDisplayValue('Bulbasaur');
+          const thirdTextInputInFirstRow =
+            screen.getAllByDisplayValue('Grass')[0];
+          const fourthTextInputInFirstRow = screen.getAllByDisplayValue(
+            'One of the three starter Pokémon'
+          )[0];
+
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(secondTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(thirdTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(fourthTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+
+          const firstTextInputInSecondRow = screen.getByDisplayValue('2');
+          expect(firstTextInputInSecondRow).toHaveFocus();
+        });
+
+        it('leaves Edit Mode when the user presses Tab on the last cell in the last row', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const lastCellButtonInLastRow = screen.getByRole('button', {
+            name: 'Evolves from Wartortle - Press Enter to edit',
+          });
+          userEvent.dblClick(lastCellButtonInLastRow);
+
+          const lastCellInputInLastRow = screen.getByDisplayValue(
+            'Evolves from Wartortle'
+          );
+          expect(lastCellInputInLastRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(document.body).toHaveFocus();
+          expect(screen.queryAllByRole('textbox').length).toBe(0);
+        });
+
+        it('moves focus left by one cell when the user presses Shift+Tab', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          const secondTextInputInFirstRow =
+            screen.getByDisplayValue('Bulbasaur');
+          const thirdTextInputInFirstRow =
+            screen.getAllByDisplayValue('Grass')[0];
+          const fourthTextInputInFirstRow = screen.getAllByDisplayValue(
+            'One of the three starter Pokémon'
+          )[0];
+
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(secondTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(thirdTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(fourthTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab({ shift: true });
+          expect(thirdTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab({ shift: true });
+          expect(secondTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab({ shift: true });
+          expect(firstTextInputInFirstRow).toHaveFocus();
+        });
+
+        it('moves focus to the last cell in the previous row when the user presses Shift+Tab on the first cell in a row', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          const secondTextInputInFirstRow =
+            screen.getByDisplayValue('Bulbasaur');
+          const thirdTextInputInFirstRow =
+            screen.getAllByDisplayValue('Grass')[0];
+          let fourthTextInputInFirstRow = screen.getAllByDisplayValue(
+            'One of the three starter Pokémon'
+          )[0];
+
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(secondTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(thirdTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+          expect(fourthTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab();
+
+          const firstTextInputInSecondRow = screen.getByDisplayValue('2');
+          expect(firstTextInputInSecondRow).toHaveFocus();
+
+          userEvent.tab({ shift: true });
+
+          fourthTextInputInFirstRow = screen.getAllByDisplayValue(
+            'One of the three starter Pokémon'
+          )[0];
+          expect(fourthTextInputInFirstRow).toHaveFocus();
+        });
+
+        it('leaves Edit Mode when the user presses Shift+Tab on the first cell in the first row', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.tab({ shift: true });
+          expect(document.body).toHaveFocus();
+          expect(screen.queryAllByRole('textbox').length).toBe(0);
+        });
+      });
+
+      describe('Enter key', () => {
+        it('moves focus down by one cell when the user presses Enter', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFirstRow, '{enter}');
+
+          const firstTextInputInSecondRow = screen.getByDisplayValue('2');
+          expect(firstTextInputInSecondRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSecondRow, '{enter}');
+
+          const firstTextInputInThirdRow = screen.getByDisplayValue('3');
+          expect(firstTextInputInThirdRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInThirdRow, '{enter}');
+
+          const firstTextInputInFourthRow = screen.getByDisplayValue('4');
+          expect(firstTextInputInFourthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFourthRow, '{enter}');
+
+          const firstTextInputInFifthRow = screen.getByDisplayValue('5');
+          expect(firstTextInputInFifthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFifthRow, '{enter}');
+
+          const firstTextInputInSixthRow = screen.getByDisplayValue('6');
+          expect(firstTextInputInSixthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSixthRow, '{enter}');
+
+          const firstTextInputInSeventhRow = screen.getByDisplayValue('7');
+          expect(firstTextInputInSeventhRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSeventhRow, '{enter}');
+
+          const firstTextInputInEighthRow = screen.getByDisplayValue('8');
+          expect(firstTextInputInEighthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInEighthRow, '{enter}');
+
+          const firstTextInputInNinthRow = screen.getByDisplayValue('9');
+          expect(firstTextInputInNinthRow).toHaveFocus();
+        });
+
+        it('leaves Edit Mode when the user presses Enter on any cell in the last row', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFirstRow, '{enter}');
+
+          const firstTextInputInSecondRow = screen.getByDisplayValue('2');
+          expect(firstTextInputInSecondRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSecondRow, '{enter}');
+
+          const firstTextInputInThirdRow = screen.getByDisplayValue('3');
+          expect(firstTextInputInThirdRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInThirdRow, '{enter}');
+
+          const firstTextInputInFourthRow = screen.getByDisplayValue('4');
+          expect(firstTextInputInFourthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFourthRow, '{enter}');
+
+          const firstTextInputInFifthRow = screen.getByDisplayValue('5');
+          expect(firstTextInputInFifthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFifthRow, '{enter}');
+
+          const firstTextInputInSixthRow = screen.getByDisplayValue('6');
+          expect(firstTextInputInSixthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSixthRow, '{enter}');
+
+          const firstTextInputInSeventhRow = screen.getByDisplayValue('7');
+          expect(firstTextInputInSeventhRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSeventhRow, '{enter}');
+
+          const firstTextInputInEighthRow = screen.getByDisplayValue('8');
+          expect(firstTextInputInEighthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInEighthRow, '{enter}');
+
+          const firstTextInputInNinthRow = screen.getByDisplayValue('9');
+          expect(firstTextInputInNinthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInNinthRow, '{enter}');
+
+          expect(document.body).toHaveFocus();
+          expect(screen.queryAllByRole('textbox').length).toBe(0);
+        });
+
+        it('moves focus up by one cell when the user presses Shift+Enter', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          let firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFirstRow, '{enter}');
+
+          let firstTextInputInSecondRow = screen.getByDisplayValue('2');
+          expect(firstTextInputInSecondRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSecondRow, '{enter}');
+
+          let firstTextInputInThirdRow = screen.getByDisplayValue('3');
+          expect(firstTextInputInThirdRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInThirdRow, '{enter}');
+
+          let firstTextInputInFourthRow = screen.getByDisplayValue('4');
+          expect(firstTextInputInFourthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFourthRow, '{enter}');
+
+          let firstTextInputInFifthRow = screen.getByDisplayValue('5');
+          expect(firstTextInputInFifthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFifthRow, '{enter}');
+
+          let firstTextInputInSixthRow = screen.getByDisplayValue('6');
+          expect(firstTextInputInSixthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSixthRow, '{enter}');
+
+          let firstTextInputInSeventhRow = screen.getByDisplayValue('7');
+          expect(firstTextInputInSeventhRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSeventhRow, '{enter}');
+
+          let firstTextInputInEighthRow = screen.getByDisplayValue('8');
+          expect(firstTextInputInEighthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInEighthRow, '{enter}');
+
+          const firstTextInputInNinthRow = screen.getByDisplayValue('9');
+          expect(firstTextInputInNinthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInNinthRow, '{shift}{enter}');
+
+          firstTextInputInEighthRow = screen.getByDisplayValue('8');
+          expect(firstTextInputInEighthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInEighthRow, '{shift}{enter}');
+
+          firstTextInputInSeventhRow = screen.getByDisplayValue('7');
+          expect(firstTextInputInSeventhRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSeventhRow, '{shift}{enter}');
+
+          firstTextInputInSixthRow = screen.getByDisplayValue('6');
+          expect(firstTextInputInSixthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSixthRow, '{shift}{enter}');
+
+          firstTextInputInFifthRow = screen.getByDisplayValue('5');
+          expect(firstTextInputInFifthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFifthRow, '{shift}{enter}');
+
+          firstTextInputInFourthRow = screen.getByDisplayValue('4');
+          expect(firstTextInputInFourthRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFourthRow, '{shift}{enter}');
+
+          firstTextInputInThirdRow = screen.getByDisplayValue('3');
+          expect(firstTextInputInThirdRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInThirdRow, '{shift}{enter}');
+
+          firstTextInputInSecondRow = screen.getByDisplayValue('2');
+          expect(firstTextInputInSecondRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInSecondRow, '{shift}{enter}');
+
+          firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          expect(firstTextInputInFirstRow).toHaveFocus();
+        });
+
+        it('leaves Edit Mode when the user presses Shift+Enter on any cell in the first row', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFirstRow, '{shift}{enter}');
+
+          expect(document.body).toHaveFocus();
+          expect(screen.queryAllByRole('textbox').length).toBe(0);
+        });
+      });
+
+      describe('Escape key', () => {
+        it('leaves Edit Mode when the user presses Escape', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.type(firstTextInputInFirstRow, '{escape}');
+
+          const firstCellInTable = screen.getByRole('button', {
+            name: '1 - Press Enter to edit',
+          });
+
+          expect(firstCellInTable).toHaveFocus();
+          expect(screen.queryAllByRole('textbox').length).toBe(0);
+        });
+      });
+
+      describe('Outside click', () => {
+        it('leaves Edit Mode when the user clicks anywhere outside of the table', () => {
+          render(<BasicEditableTable tableData={basicPokemonData} />);
+          enterEditMode();
+
+          const firstTextInputInFirstRow = screen.getByDisplayValue('1');
+          expect(firstTextInputInFirstRow).toHaveFocus();
+
+          userEvent.click(document.body);
+
+          expect(document.body).toHaveFocus();
+          expect(screen.queryAllByRole('textbox').length).toBe(0);
+        });
+      });
     });
   });
 });
